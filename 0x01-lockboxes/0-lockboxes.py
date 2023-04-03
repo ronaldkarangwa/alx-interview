8de852d7ea32a82253c2c8a609930103066fc7ee
ronald <gahima.ronald@yahoo.com>
2023-04-03 16:09:04 +0200

    interview alx

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,43 +1,17 @@
 #!/usr/bin/python3
-
-"""
-
-    This module contains the canUnlockAll function.
-
-"""
-
-
-
+"""Script will unlock list of lists"""
 
 
 def canUnlockAll(boxes):
-
-    """Method that determines if all the boxes can be opened
-
-        Arg:
-
-            boxes: Is a list of lists.
-
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
     """
 
-    box = len(boxes) * [False]
-
-    box[0] = True
-
     keys = [0]
-
-
-
-    for each in keys:
-
-        for i in boxes[each]:
-
-            if i not in keys:
-
-                if i < len(boxes):
-
-                    box[i] = True
-
-                    keys.append(i)
-
-    return all(box)
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
